99974b9a395a994048f530c4a6152a0286071cd7
Muhamad Nahrowi <86625224+m-nahrowi@users.noreply.github.com>
2024-09-23 16:48:26 +0700

    upload tugas

---
+++ tugas-week-1.dart
@@ -0,0 +1,34 @@
+// Muhamad Nahrowi
+
+void main() {
+  // panggil fungsi kalkulator
+  print("kalkulator");
+  kalkulator(4, 3, "+");
+  kalkulator(7, 3, "-");
+  kalkulator(9, 3, "*");
+  kalkulator(15, 3, "/");
+  print("=" * 30);
+
+  // perulangan for 1 - 20
+  print("Bilangan genap");
+  for (int i = 1; i <= 20; i++) {
+    if (i % 2 == 0) {
+      print(i);
+    }
+  }
+}
+
+// kalkulator
+void kalkulator(double number, double number2, String operator) {
+  double result = 0;
+  if (operator == "+") {
+    result = number + number2;
+  } else if (operator == "-") {
+    result = number - number2;
+  } else if (operator == "*") {
+    result = number * number2;
+  } else if (operator == "/") {
+    result = number / number2;
+  }
+  print("$number $operator $number2 = $result");
+}
